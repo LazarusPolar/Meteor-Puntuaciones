93c8652ef045a91713a283e2336115173478bdb5
LazarusPolar <roberto.almaraz@habilgroup.net>
2018-01-18 23:22:57 -0600

    Se hacen modificaciones a las declaraciones de exports. Se agrega ejemplo de 'Default Export'

--- imports/utils.js
+++ imports/utils.js
@@ -1,5 +1,8 @@
 console.log('Log desde /imports/utils.js');
 
+let saludo = 'Hola'
+export default saludo;
+
 // La palabra 'export' hace que la funcion se exporte 
 export let saludaUsuario = function () {
     return 'Hola, usuario!';
--- server/main.js
+++ server/main.js
@@ -1,6 +1,8 @@
 // ./ (Importa archivo local)
 //import './../imports/utils'
 import {saludaUsuario} from './../imports/utils'
+import suma from './../imports/add'
 
 console.log('Log from /server/main.js')
 console.log(saludaUsuario())
+console.log(suma(10,10))
