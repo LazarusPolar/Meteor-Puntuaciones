6392bbef48d6149f79cfc0940b2f93f6fe295556
LazarusPolar <roberto.almaraz@habilgroup.net>
2018-01-28 18:14:54 -0600

    Agrega archivo API para insercion de datos en MongoDB

---
+++ imports/api/jugadores.js
@@ -0,0 +1,6 @@
+// Para crear colecciones en MongoDB
+import {Mongo} from 'meteor/mongo'
+
+// Funcion constructora
+export const Jugadores = new Mongo.Collection('jugadores')
+
--- server/main.js
+++ server/main.js
@@ -3,6 +3,25 @@
 import {saludaUsuario} from './../imports/utils'
 import suma from './../imports/suma'
 
+import {Meteor} from 'meteor/meteor'
+// Se importa la funcion de Jugadores 
+import {Jugadores} from '.././imports/api/jugadores'
+
+// Funcion ejecutada al iniciar el proceso del servidor
+Meteor.startup(function () {
+    Jugadores.insert({
+        nombre: 'Alemania2', 
+        puntos: 30
+    })
+
+    Jugadores.insert({
+        nombre: 'Rugal', 
+        puntos: 30
+    })
+    // Retorna un puntero, por eso se le agrega "fetch()"
+    console.log(Jugadores.find().fetch())
+})
+
 console.log('Log from /server/main.js')
 console.log(saludaUsuario())
 console.log(suma(10,10))
